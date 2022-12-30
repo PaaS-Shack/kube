bff3767420236b6570361452fe98969e965aaaf5
FLYBYME <mangoraft@gmail.com>
2022-12-30 11:16:42 -0500

    Added limitrange

--- services/kube.service.js
+++ services/kube.service.js
@@ -219,7 +219,7 @@ const api = [{
 	methods
 }
 ]
-const core = ['pods', 'endpoints', 'services', 'persistentvolumeclaims', 'events', 'nodes', 'resourcequotas', 'namespaces', 'limitrange']
+const core = ['pods', 'endpoints', 'services', 'persistentvolumeclaims', 'events', 'nodes', 'resourcequotas', 'namespaces', 'limitranges']
 const apps = ['replicasets', 'deployments', 'statefulsets', 'daemonsets']
 const batch = ['jobs', 'cronjobs']
 /**
